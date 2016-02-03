5cd5142db7c644a0886f779bb0ba1d89d0a5447f
Christoph Halang <christoph.halang@gmail.com>
2016-02-03 20:33:24 +0100

    client call to sever

--- container_client/client.js
+++ container_client/client.js
@@ -1,14 +1,8 @@
 var http = require("http");
-
-//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-var options = {
-  host: 'www.random.org',
-  path: '/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-};
-
+ 
 var request = require('request');
-request('http://www.google.com', function (error, response, body) {
+request('http://localhost:8888', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage.
+    console.log("client received response from server: "+body)
   }
 })
