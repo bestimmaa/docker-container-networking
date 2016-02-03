59eaa601b4f8216b8c2a74eb2de1a001fd447177
Christoph Halang <christoph.halang@gmail.com>
2016-02-03 21:17:04 +0100

    client loops 1000ms and prints error

--- container_client/client.js
+++ container_client/client.js
@@ -1,8 +1,16 @@
 var http = require("http");
 console.log("Starting up client :)")
 var request = require('request');
-request('http://localhost:8888', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log("client received response from server: "+body)
-  }
-})
+
+
+setInterval(function(){
+  console.log('test');
+  request('http://localhost:8888', function (error, response, body) {
+    if(error){
+      console.log("error: "+error);
+    }
+    if (!error && response.statusCode == 200) {
+      console.log("client received response from server: "+body);
+    }
+  });
+},1000);
