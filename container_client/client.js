476570c237bb1faceb5138695d353649a71b625e
Christoph Halang <christoph.halang@gmail.com>
2016-02-03 20:56:44 +0100

    dockerized

--- container_client/client.js
+++ container_client/client.js
@@ -1,5 +1,5 @@
 var http = require("http");
- 
+console.log("Starting up client :)")
 var request = require('request');
 request('http://localhost:8888', function (error, response, body) {
   if (!error && response.statusCode == 200) {
