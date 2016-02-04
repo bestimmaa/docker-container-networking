2b865f329aed36707cbeb1b9ae163e66d425090b
Christoph Halang <christoph.halang@gmail.com>
2016-02-04 20:53:07 +0100

    cosmetics

--- client/client.js
+++ client/client.js
@@ -6,17 +6,19 @@ var server_port = process.env.SIMPLE_SERVER_PORT_8888_TCP_PORT
 console.log("Server address is set to "+server_ip+":"+server_port)
 
 var request = require('request');
+var server_address = 'http://'+server_ip+":"+server_port;
 
 setInterval(function(){
-  request('http://'+server_ip+":"+server_port, function (error, response, body) {
+  console.log("sending http request...");
+  request(server_address, function (error, response, body) {
     if(error){
       console.log("error: "+error);
     }
     else if (!error && response.statusCode == 200) {
-      console.log("client received response from server: "+body);
+      console.log("received code 200 response: "+body);
     }
     else{
-      console.log("unkown response :"+body);
+      console.log("unkown response:"+body);
     }
   });
 },1000);
